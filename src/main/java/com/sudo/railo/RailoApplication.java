cac0a03e64d7636cc56d18e6b14e88d62bf217d0
parkjin <aac@kakao.com>
2025-07-01 10:59:26 +0900

    payment누락 및 추가

--- src/main/java/com/sudo/railo/RailoApplication.java
+++ src/main/java/com/sudo/railo/RailoApplication.java
@@ -4,12 +4,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableScheduling
+@EnableAsync  // 이벤트 처리용
 @EnableJpaAuditing
+
+// JPA Repository 패키지 명시적 지정 (Redis 패키지 제외)
+@EnableJpaRepositories(basePackages = {
+    "com.sudo.railo.payment.infrastructure.persistence",
+    "com.sudo.railo.booking.infra",
+    "com.sudo.railo.member.infra",
+    "com.sudo.railo.train.infrastructure"
+})
+
+// Redis Repository 패키지 별도 지정 (JPA와 분리)
+@EnableRedisRepositories(basePackages = {
+    "com.sudo.railo.global.redis"
+})
 public class RailoApplication {
 
 	public static void main(String[] args) {
